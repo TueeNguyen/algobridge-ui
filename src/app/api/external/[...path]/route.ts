de1758ec63bfd813bba0292c74c9f3e29f1b5493
dev-khoi <daknguyen111@gmail.com>
2025-12-26 20:25:58 -0500

    fix: add in allow origin from frontend (for fetch request from next.js server)

--- src/app/api/external/[...path]/route.ts
+++ src/app/api/external/[...path]/route.ts
@@ -46,6 +46,9 @@ const proxyRequest = async (
       headers: {
         "Content-Type":
           response.headers.get("Content-Type") || "application/json",
+        "Access-Control-Allow-Origin": process.env.NEXT_PUBLIC_BASE_URL || "",
+       "Access-Control-Allow-Methods": "GET,POST,PUT,PATCH,DELETE,OPTIONS",
+       "Access-Control-Allow-Headers": "*",
       },
     });
   } catch (error) {
